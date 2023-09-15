e7e6956a3b024bef7300b76a8f353a4469fbc486
Akshay-Masal <145155784+Akshay-Masal@users.noreply.github.com>
2023-09-15 20:01:35 +0000

    code for variable

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+const rollNo = 111;
+let studentName = "Akshay Masal";
+let studentEmail = "akshaymasal@gmail.com"
+var mobileNo = '0987654321';
+studentCity = 'Pune';
+
+//rollNo = 2; //TypeError: Assignment to constant variable. Means not allowed.
+
+console.log(mobileNo);
+console.table([rollNo, studentName, studentEmail, mobileNo, studentCity]);
